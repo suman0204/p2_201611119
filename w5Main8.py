4003d706f54be9d88854671aa9a59a223f19dd5a
suman0204 <suman0204@naver.com>
2016-04-04 17:46:48 +0900

    w5Main8

---
+++ w5Main8.py
@@ -0,0 +1,14 @@
+﻿height=input ("input user height (m) : ")
+weight=input ("input user weight (kg): ")
+print "%s" %height, "%s" %weight
+BMI=weight/(height*height)
+print "%s" %BMI
+if BMI<18.5:
+     print 'Low weight'
+elif 18.5<= BMI <23:
+     print 'normal weight'
+
+elif 23<= BMI <25:
+       print 'over weight'
+else:
+    print 'very over weight'
