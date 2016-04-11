2454952e19bdbe929cce6922957695fc45cb616e
suman0204 <suman0204@naver.com>
2016-04-11 17:11:57 +0900

    w6 sum of 4 and not 5

---
+++ w6 sum of 4 and not 5.py
@@ -0,0 +1,25 @@
+﻿def sumList(x):
+    x=list()
+    for i in range(1,1000):
+        if i%4==0 and not i%5==0:
+            x.append(i)
+            
+    sum=0
+    
+    for i in range(0,len(x)):
+        sum+=x[i]
+    return sum
+print sum
+
+
+def lab6():
+    print """w6 p2 201611119 homework"""
+    labsum=sumList(x)
+    print labsum
+    
+def main():
+    lab6()
+    
+
+
+main()
