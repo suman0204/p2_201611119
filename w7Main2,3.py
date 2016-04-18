145ec3f171ac18cf1bd5c5bb0acb970da108a1dd
suman0204 <suman0204@naver.com>
2016-04-18 17:24:45 +0900

    w7Main2,3
    거북이 트랙을 저장하기
    도형을 데이터구조로 저장하고 그리기

---
+++ w7Main2,3.py
@@ -0,0 +1,50 @@
+﻿import turtle
+t1=turtle.Turtle()
+wn=turtle.Screen()
+
+
+def drawSquareAtSave(size, pos):
+    t1.penup()
+    t1.setpos(pos)
+    t1.pendown()
+    tracks=list()
+    for i in range(0,4):
+        tracks.append(t1.pos())
+        t1.forward(size)
+        t1.right(90)
+    return tracks
+
+
+def drawSquareForm(tracks):
+    tracks=dict()
+    tracks=({0:(0,0),1:(100,0),2:(100,100),3:(0,100),4:(0,0)})
+
+    for i in range(1,5):
+        t1.goto(tracks[i])
+    for j in range(1,5):
+        print tracks[j]
+
+
+
+def lab7b():
+    mytrack=drawSquareAt(size,pos)
+    print mytracks
+
+def lab7c():
+    drawSquareForm(tracks)
+    
+
+    
+    
+def main():
+    lab7b()
+    lab7c()
+    
+if __name__== "__main__":
+       main()
+    
+wn.exitonclick()
+
+main()
+
+wn.exitonclick()
