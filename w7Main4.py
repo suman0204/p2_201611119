9d5fb0b0ebcf60da116766cc12f16b37fba8922a
suman0204 <suman0204@naver.com>
2016-04-20 13:43:10 +0900

    거북이가 걸어간 트랙

---
+++ w7Main4.py
@@ -0,0 +1,72 @@
+﻿def savTracks():
+    import turtle
+    wn=turtle.Screen()
+    wn.bgpic("myMaze.gif")
+    t1=turtle.Turtle()
+    t1.speed(1)
+    t1.penup()
+
+    mytracks=list()
+
+    t1.goto(-400,300)
+
+    mytracks.append(t1.pos())
+
+    t1.pendown()
+    t1.pencolor("Red")
+    t1.right(90)
+    t1.fd(400)
+
+    mytracks.append(t1.pos())
+
+    t1.backward(150)
+    t1.left(90)
+    t1.fd(300)
+
+    mytracks.append(t1.pos())
+
+    t1.left(90)
+    t1.fd(300)
+
+    mytracks.append(t1.pos())
+
+    t1.back(150)
+
+    mytracks.append(t1.pos())
+
+    t1.right(90)
+    t1.fd(300)
+
+    mytracks.append(t1.pos())
+
+    t1.left(90)
+    t1.right(90)
+    t1.right(90)
+    t1.fd(200)
+
+    mytracks.append(t1.pos())
+
+    t1.fd(300)
+
+    mytracks.append(t1.pos())
+
+    t1.back(100)
+
+    mytracks.append(t1.pos())
+
+    t1.left(90)
+    t1.fd(200)
+    mytracks.append(t1.pos())
+    
+    return mytracks
+
+def replayTracks(mytracks):
+    for t in mytracks:
+        print t
+
+def lab7():
+    mytracks=saveTracks()
+    replayTracks(mytracks)
+    
+def main():
+    lab7()
