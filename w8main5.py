12f03c2e1216ced3a31477bb5e49ec0fc1cc35b2
suman0204 <suman0204@naver.com>
2016-05-02 15:23:51 +0900

    함수로 만들어서 다시 올립니다

--- w8main5.py
+++ w8main5.py
@@ -1,20 +1,32 @@
-﻿sentence='sangmyung university'
-d=dict()
-for c in sentence:
-    if c not in d:
-        d[c]=1
-        print d
-    else:
-        d[c]=d[c]+1
-print d
+﻿def charCount(word):
+    global d
+    d=dict()
+    for c in sentence:
+        if c not in d:
+            d[c]=1
+            print d
+        else:
+            d[c]=d[c]+1
+    return d
+    
+def drawBar():
+    
+    import matplotlib
+    import matplotlib.pyplot as plt
 
-
-%matplotlib inline
-
-import matplotlib
-import matplotlib.pyplot as plt
-
-plt.bar(range(len(d)),d.values(), align='center')
-plt.xticks(range(len(d)), list(d.keys()))
-plt.show()
-
+    plt.bar(range(len(d)),d.values(), align='center')
+    plt.xticks(range(len(d)), list(d.keys()))
+    plt.show()
+    
+def lab8():
+    global word
+    word='sangmyung university'
+    result=charCount(word)
+    print result
+    drawBar()
+    
+def main():
+    lab8()
+    
+if__name__=="__main__":
+    main()			
