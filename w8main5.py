55a6bf032bdddd2a28f606a6223b276a559f77bd
suman0204 <suman0204@naver.com>
2016-05-02 15:06:55 +0900

    문자열에서 문자를 세기

---
+++ w8main5.py
@@ -0,0 +1,20 @@
+﻿sentence='sangmyung university'
+d=dict()
+for c in sentence:
+    if c not in d:
+        d[c]=1
+        print d
+    else:
+        d[c]=d[c]+1
+print d
+
+
+%matplotlib inline
+
+import matplotlib
+import matplotlib.pyplot as plt
+
+plt.bar(range(len(d)),d.values(), align='center')
+plt.xticks(range(len(d)), list(d.keys()))
+plt.show()
+
