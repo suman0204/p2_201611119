5cdbc0accf0b015ff3a1e4b642a25693079c707b
suman0204 <suman0204@naver.com>
2016-05-13 14:26:39 +0900

    w11 만족도 평균과 대통령 연설문 과제

---
+++ w11assignment.py
@@ -0,0 +1,183 @@
+﻿def GeorgeBushSpeech():
+    GeorgeBush=list()
+    GeorgeBush=["vice President Cheney Mr Chief Justice President Carter President Bush President Clinton reverend clergy distinguished guests fellow citizens",
+"on this day prescribed by law and marked by ceremony we celebrate the durable wisdom of our constitution and recall the deep commitments that unite our countr" ,"I am grateful for the honor of this hour mindful of the consequential times in which we live and determined to fulfill the oath that I have sworn and you have witnessed ",
+
+"at this second gathering our duties are defined not by the words I use but by the history we have seen together" ,"for a half century America defended our own freedom by standing watch on distant borders","after the shipwreck of communism came years of relative quiet years of repose years of sabbatical and then there came a day of fire",
+
+"we have seen our vulnerability and we have seen its deepest source"," for as long as whole regions of the world simmer in resentment and tyranny prone to ideologies that feed hatred and excuse murder violence will gather and multiply in destructive power and cross the most defended borders and raise a mortal threat"," there is only one force of history that can break the reign of hatred and resentment and expose the pretensions of tyrants and reward the hopes of the decent and tolerant and that is the force of human freedom",
+
+"we are led by events and common sense to one conclusion"," the survival of liberty in our land increasingly depends on the success of liberty in other lands"," the best hope for peace in our world is the expansion of freedom in all the world",
+
+"america's vital interests and our deepest beliefs are now one","from the day of our founding we have proclaimed that every man and woman on this earth has rights and dignity and matchless value because they bear the image of the maker of heaven and earth"," across the generations we have proclaimed the imperative of self government because no one is fit to be a master and no one deserves to be a slave"," advancing these ideals is the mission that created our Nation"," it is the honorable achievement of our fathers"," Now it is the urgent requirement of our nation's security and the calling of our time",
+
+"so it is the policy of the United States to seek and support the growth of democratic movements and institutions in every nation and culture with the ultimate goal of ending tyranny in our world",
+
+"this is not primarily the task of arms though we will defend ourselves and our friends by force of arms when necessary","freedom by its nature must be chosen and defended by citizens and sustained by the rule of law and the protection of minorities","and when the soul of a nation finally speaks the institutions that arise may reflect customs and traditions very different from our own","America will not impose our own style of government on the unwilling","our goal instead is to help others find their own voice attain their own freedom and make their own way",
+
+"the great objective of ending tyranny is the concentrated work of generations","the difficulty of the task is no excuse for avoiding it","America's influence is not unlimited but fortunately for the oppressed America's influence is considerable and we will use it confidently in freedom's cause",
+
+"my most solemn duty is to protect this nation and its people against further attacks and emerging threats","some have unwisely chosen to test America's resolve and have found it firm",
+
+"we will persistently clarify the choice before every ruler and every nation", "the moral choice between oppression which is always wrong and freedom which is eternally right","America will not pretend that jailed dissidents prefer their chains or that women welcome humiliation and servitude or that any human being aspires to live at the mercy of bullies",
+
+"we will encourage reform in other governments by making clear that success in our relations will require the decent treatment of their own people"," America's belief in human dignity will guide our policies yet rights must be more than the grudging concessions of dictators they are secured by free dissent and the participation of the governed","in the long run there is no justice without freedom and there can be no human rights without human liberty",
+
+"some I know have questioned the global appeal of liberty though this time in history four decades defined by the swiftest advance of freedom ever seen is an odd time for doubt","Americans of all people should never be surprised by the power of our ideals","eventually the call of freedom comes to every mind and every soul","we do not accept the existence of permanent tyranny because we do not accept the possibility of permanent slavery","liberty will come to those who love it",
+
+"today America speaks anew to the peoples of the world",
+"all who live in tyranny and hopelessness can know the United States will not ignore your oppression or excuse your oppressors","when you stand for your liberty we will stand with you",
+
+"democratic reformers facing repression prison or exile can know America sees you for who you are the future leaders of your free country",
+
+"the rulers of outlaw regimes can know that we still believe as Abraham Lincoln did " "those who deny freedom to others deserve it not for themselves and under the rule of a just god cannot long retain it ",
+
+"the leaders of governments with long habits of control need to know to serve your people you must learn to trust them", "start on this journey of progress and justice and America will walk at your side",
+
+"and all the allies of the United States can know we honor your friendship we rely on your counsel and we depend on your help","division among free nations is a primary goal of freedom's enemies","the concerted effort of free nations to promote democracy is a prelude to our enemies' defeat",
+
+"today I also speak anew to my fellow citizens",
+
+"from all of you I have asked patience in the hard task of securing America which you have granted in good measure","our country has accepted obligations that are difficult to fulfill and would be dishonorable to abandon","yet because we have acted in the great liberating tradition of this nation tens of millions have achieved their freedom","and as hope kindles hope millions more will find it","by our efforts we have lit a fire as well a fire in the minds of men","it warms those who feel its power it burns those who fight its progress and one day this untamed fire of freedom will reach the darkest corners of our world",
+
+"a few Americans have accepted the hardest duties in this cause in the quiet work of intelligence and diplomacy the idealistic work of helping raise up free governments the dangerous and necessary work of fighting our enemies","some have shown their devotion to our country in deaths that honored their whole lives and we will always honor their names and their sacrifice",
+
+"all Americans have witnessed this idealism and some for the first time", "I ask our youngest citizens to believe the evidence of your eyes","you have seen duty and allegiance in the determined faces of our soldiers","you have seen that life is fragile and evil is real and courage triumphs","make the choice to serve in a cause larger than your wants larger than yourself and in your days you will add not just to the wealth of our country but to its character",
+
+"america has need of idealism and courage because we have essential work at home the unfinished work of American freedom","in a world moving toward liberty we are determined to show the meaning and promise of liberty",
+
+"in America's ideal of freedom citizens find the dignity and security of economic independence instead of laboring on the edge of subsistence","this is the broader definition of liberty that motivated the homestead acta the social security act and the G.I. bill of rights","and now we will extend this vision by reforming great institutions to serve the needs of our time","to give every American a stake in the promise and future of our country we will bring the highest standards to our schools and build an ownership society","we will widen the ownership of homes and businesses retirement savings and health insurance preparing our people for the challenges of life in a free society","by making every citizen an agent of his or her own destiny we will give our fellow Americans greater freedom from want and fear and make our society more prosperous and just and equal",
+
+"in America's ideal of freedom the public interest depends on private character on integrity and tolerance toward others and the rule of conscience in our own lives","self government relies in the end on the governing of the self","that edifice of character is built in families supported by communities with standards and sustained in our national life by the truths of Sinai the Sermon on the Mount the words of the Koran and the varied faiths of our people","americans move forward in every generation by reaffirming all that is good and true that came before ideals of justice and conduct that are the same yesterday today and forever",
+"in America's ideal of freedom the exercise of rights is ennobled by service and mercy and a heart for the weak", "liberty for all does not mean independence from one another", "our nation relies on men and women who look after a neighbor and surround the lost with love","Americans at our best value the life we see in one another and must always remember that even the unwanted have worth","and our country must abandon all the habits of racism because we cannot carry the message of freedom and the baggage of bigotry at the same time",
+"from the perspective of a single day including this day of dedication the issues and questions before our country are many", "from the viewpoint of centuries the questions that come to us are narrowed and few","did our generation advance the cause of freedom ?","and did our character bring credit to that cause ?", 
+
+"these questions that judge us also unite us because Americans of every party and background Americans by choice and by birth are bound to one another in the cause of freedom","we have known divisions which must be healed to move forward in great purposes and I will strive in good faith to heal them", "yet those divisions do not define America", "we felt the unity and fellowship of our nation when freedom came under attack and our response came like a single hand over a single heart","and we can feel that same unity and pride whenever America acts for good and the victims of disaster are given hope and the unjust encounter justice and the captives are set free",
+
+"we go forward with complete confidence in the eventual triumph of freedom","not because history runs on the wheels of inevitability it is human choices that move events","not because we consider ourselves a chosen nation god moves and chooses as he wills", "we have confidence because freedom is the permanent hope of mankind the hunger in dark places the longing of the soul","when our founders declared a new order of the ages when soldiers died in wave upon wave for a union based on liberty when citizens marched in peaceful outrage under the banner freedom now they were acting on an ancient hope that is meant to be fulfilled", "history has an ebb and flow of justice but history also has a visible direction set by liberty and the author of liberty",
+
+"when the declaration of independence was first read in public and the liberty Bell was sounded in celebration a witness said It rang as if it mean",
+"may god bless you and may he watch over the United States of America"]
+    doc=GeorgeBush
+    d=dict() 
+    for i in range(len(doc)): 
+        for c in doc[i].split(): 
+            if c not in d: 
+                d[c]=1 
+             
+            else: 
+                d[c]+=1 
+
+    for s in range(len(d)):
+        if d.values()[s]>24:
+            print d.keys()[s],d.values()[s]
+    a=list() 
+    for i in d.values(): 
+        a.append(i) 
+    b=list()
+    c=list()
+    for i in d.keys(): 
+        b.append(i) 
+    for i in range(len(a)): 
+        if a[i]>=24: 
+            c.append(b[i])
+    print "GeorgeBush was likely to say words "+str(c)+" in his speech"
+
+
+
+def RooseveltSpeech():
+    Roosevelt=list()
+    Roosevelt=["Mr. Chief Justice Mr. Vice President my friends you will understand and I believe agree with my wish that the form of this inauguration be simple and its words brief",
+
+"we Americans of today together with our allies are passing through a period of supreme test", "it is a test of our courage of our resolve of our wisdom our essential democracy",
+
+"if we meet that test successfully and honorably we shall perform a service of historic importance which men and women and children will honor throughout all time",
+
+"as I stand here today having taken the solemn oath of office in the presence of my fellow countrymen in the presence of our god I know that it is America's purpose that we shall not fail",
+
+"in the days and in the years that are to come we shall work for a just and honorable peace a durable peace as today we work and fight for total victory in war",
+
+"we can and we will achieve such a peace",
+
+"we shall strive for perfection we shall not achieve it immediately but we still shall strive", "we may make mistakes but they must never be mistakes which result from faintness of heart or abandonment of moral principle",
+
+"I remember that my old schoolmaster Dr. Peabody said in days that seemed to us then to be secure and untroubled", "things in life will not always run smoothly", "sometimes we will be rising toward the heights then all will seem to reverse itself and start downward","the great fact to remember is that the trend of civilization itself is forever upward that a line drawn through the middle of the peaks and the valleys of the centuries always has an upward trend",
+
+"our constitution of 1787 was not a perfect instrument it is not perfect yet", "but it provided a firm base upon which all manner of men of all races and colors and creeds could build our solid structure of democracy",
+
+"and so today in this year of war 1945 we have learned lessons at a fearful cost and we shall profit by them",
+
+"we have learned that we cannot live alone at peace that our own well being is dependent on the well being of other nations far away","we have learned that we must live as men not as ostriches nor as dogs in the manger",
+
+"we have learned to be citizens of the world members of the human community",
+
+"we have learned the simple truth  as Emerson said  that the only way to have a friend is to be one",
+
+"we can gain no lasting peace if we approach it with suspicion and mistrust or with fear","we can gain it only if we proceed with the understanding the confidence and the courage which flow from conviction",
+
+"the almighty god has blessed our land in many ways","he has given our people stout hearts and strong arms with which to strike mighty blows for freedom and truth","he has given to our country a faith which has become the hope of all peoples in an anguished world",
+
+"so we pray to him now for the vision to see our way clearly to see the way that leads to a better life for ourselves and for all our fellow men to the achievement of his will to peace on earth"]
+    doc=Roosevelt
+    d=dict() 
+    for i in range(len(doc)): 
+        for c in doc[i].split(): 
+            if c not in d: 
+                d[c]=1 
+             
+            else: 
+                d[c]+=1 
+
+    for s in range(len(d)):
+        if d.values()[s]>6:
+            print d.keys()[s],d.values()[s]
+    a=list() 
+    for i in d.values(): 
+        a.append(i) 
+    b=list()
+    c=list()
+    for i in d.keys(): 
+        b.append(i) 
+    for i in range(len(a)): 
+        if a[i]>=5: 
+            c.append(b[i])
+    print "Roosevelt was likely to say words "+str(c)+" in his speech"
+
+
+
+def PresidentSpeech():
+    GeorgeBushSpeech()
+    RooseveltSpeech()
+
+
+def Satisfaction():
+    sa=list()
+    sa=[["Division","Very satisfied","satisfied","dissatisfied","Very dissatisfied"],["Class quality",13.1,37.1,8.7,1.5
+        ],["teaching method",10.6,34.6,13.4,1.9],["Friendship",27.1,40.0,2.9,1.5],["Teacher relation",16.2,37.8,6.8,0.8],
+       ["School facility",11.4,29.8,14.8,4.9],["School environment",12.2,26.5,14.9,4.4],
+        ["Major",13.5,29.7,11.1,2.4],["School life",13.7,37.6,4.1,1.2]]
+    sa2=sa[1:]
+    sum=0
+    for i in sa2:
+        sum1=i[1]+i[2]
+        sum+=sum1
+    sum2=0
+    for i in sa2:
+        sum3=i[3]+i[4]
+        sum2+=sum3
+    SatisfiedAverage=sum/len(sa2)
+    DissatisfiedAverage=sum2/len(sa2)
+    
+    print "Satisfied average is "+str(SatisfiedAverage)
+    print "Dissatisfied average is "+str(DissatisfiedAverage)
+
+
+def lab11():
+    PresidentSpeech()
+    Satisfaction()
+
+def main():
+    lab11()
+
+if __name__=="__main__": 
+    main() 
