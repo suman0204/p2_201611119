d159e8c9312b8a5bb25bb79d1d222aee4f9b040d
suman0204 <suman0204@naver.com>
2016-05-16 14:45:14 +0900

    이벤트-3 과제

---
+++ w11main-3.py
@@ -0,0 +1,35 @@
+﻿import turtle
+wn=turtle.Screen()
+wn.bgpic("C:\Users\P400\Desktop/myMaze.gif")
+t1=turtle.Turtle()
+t1.shape("turtle")
+t1.speed(1)
+t1.penup()
+t1.goto(-320,300)
+t1.clear()
+t1.pendown()
+
+def keyup():
+     t1.fd(50)
+def keydown():
+     t1.back(50)
+def keyright():
+     t1.right(45)
+def keyleft():
+     t1.left(45)
+def addkeys():
+    wn.onkey(keyup,"Up")
+    wn.onkey(keydown,"Down")
+    wn.onkey(keyright,"Right")
+    wn.onkey(keyleft,"Left")
+def mousegoto(x,y):
+     t1.setpos(x,y)
+        
+def addmouse():
+     wn.onclick(mousegoto)
+
+
+addkeys()
+addmouse()
+wn.listen()
+turtle.mainloop()
