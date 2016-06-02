5f72a44cce1258b493986de009c19b8bdf62b6ea
suman0204 <suman0204@naver.com>
2016-06-02 18:25:47 +0900

    강아지소리 과제 입니다.

---
+++ w14Main.py
@@ -0,0 +1,43 @@
+﻿class Dog(object):
+    def __init__(self,name):
+        self.name=name
+    def getName(self):
+        print "my dog name is ",self.name
+    def talk(self):
+        print "mung mung"
+        
+class ShihTzuDog(object):
+    def __init__(self,name):
+        self.name=name
+    def getName(self):
+        print "my dog name is ",self.name
+    def talk(self):
+        print "si si"
+
+class Maltese(object):
+    def __init__(self,name):
+        self.name=name
+    def getName(self):
+        print "my dog name is ",self.name
+    def talk(self):
+        print "mal mal"
+        
+def dogSound():
+    mydog=Dog('poppy')
+    mydog.getName()
+    mydog.talk()
+    Shi=ShihTzuDog('ShihTzu')
+    Shi.getName()
+    Shi.talk()
+    Mal=Maltese('Maltese')
+    Mal.getName()
+    Mal.talk()
+    
+def lab14():
+    dogSound()
+
+def main():
+    lab14()
+    
+if __name__=="__main__":
+    main()
